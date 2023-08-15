92e83abc8690e7b37c781856866469c4c114df78
Patrick McCaffrey <62127532+swinPatrick@users.noreply.github.com>
2023-08-15 16:52:55 +1000

    Update MultiplierMultiCycle.vhd

--- MultiplierMultiCycle.vhd
+++ MultiplierMultiCycle.vhd
@@ -6,9 +6,9 @@ use ieee.std_logic_unsigned.all;
 entity MultiplierMultiCycle is
     Port ( Clock    : in  std_logic;
            Reset    : in  std_logic;
-           A        : in  std_logic_vector(15 downto 0);
-           B        : in  std_logic_vector(15 downto 0);
-           Q        : out std_logic_vector(31 downto 0);
+           A        : in  unsigned(15 downto 0);
+           B        : in  unsigned(15 downto 0);
+           Q        : out unsigned(31 downto 0);
            Start    : in  std_logic;
            Complete : out std_logic);
 end MultiplierMultiCycle;
@@ -18,7 +18,11 @@ architecture Behavioral of MultiplierMultiCycle is
 type theStates is (IdleState, Stage1, Stage2, Stage3, Stage4); 
 signal state : theStates;
 -- variable signal for the result of the 8 bit multiplier
-signal Product : std_logic_vector(15 downto 0);
+-- possibly use signal to avoid latches
+signal Result : unsigned(31 downto 0); -- The result of the 16x16 multiplication
+signal Product : unsigned(15 downto 0); -- The result of the 8x8 multiplication
+signal LHS, RHS : unsigned(7 downto 0); -- The left and right hand side of the 8x8 multiplication
+
 
 begin
    --
@@ -32,7 +36,6 @@ begin
    StateMachine: -- State machine
    process (reset, clock)
    begin
-
       if (reset = '1') then
          state    <= IdleState;
          Complete <= '0';
@@ -59,66 +62,74 @@ begin
       end if;
  
    end process StateMachine;
+   
 
    --
    -- Asynchronous elements of the data path
    -- 
    Comb: -- combinational paths
-   process( state, A, B )
+   process( state )
+      -- watching for changes in state, A, B
+      -- change values of LHS and RHS depernding on the state
    begin
       case state is
-         null;
-      case Stage1 =>
-         Product <= A(7 downto 0) * B(7 downto 0);
-      case Stage2 =>
-         Product <= A(7 downto 0) * B(15 downto 8);
-      case Stage3 =>
-         Product <= A(15 downto 8) * B(7 downto 0);
-      case Stage4 =>
-         Product <= A(15 downto 8) * B(15 downto 8);
+      when IdleState | Stage1 =>
+         LHS <= A(7 downto 0);
+         RHS <= B(7 downto 0);
+      when Stage2 =>
+         LHS <= A(7 downto 0);
+         RHS <= B(15 downto 8);
+      when Stage3 =>
+         LHS <= A(15 downto 8);
+         RHS <= B(7 downto 0);
+      when Stage4 =>
+         LHS <= A(15 downto 8);
+         RHS <= B(15 downto 8);
       end case;
    end process Comb;
 
 
    --
    -- Synchronous elements of the data path
-   -- builds output and flip flops/registers
+   -- 
    Synch: -- synchronous elements
    process (reset, clock)
-      variable LHS, RHS : std_logic_vector(7 downto 0);
-      variable Result : std_logic_vector(31 downto 0);
-
    begin
       if (reset = '1') then
-         Q <= (others => '0');
-      elsif (clock'event and (clock = '1')) then
+         Result <= (others => '0');
+      elsif rising_edge(clock) then
          case state is
             when IdleState =>
-               null;
+                null;
             -- Multiply seccond byte of A and B
             when Stage1 =>
                -- set first half of Result is 0, seccond half is Multipe
-               Result := (others => '0');
-               Result(15 downto 0) := Product;
+               Result <= (others => '0');
+               Result(15 downto 0) <= Product;
 
             -- Multiply seccond byte of A with first byte of B
             when Stage2 =>
                -- add existing value of Q(15 downto 8) with Multiple. Overflow is not possible
-               Result(23 downto 8) := Result(23 downto 8) + Product;
+               --Result <= Result + (x"00" & Product & x"00");
+               Result(24 downto 8) <= Result(24 downto 8) + Product;
 
             -- Multiply first byte of A with second byte of B
             when Stage3 =>
                -- add existing value of Q(15 downto 8) with Multiple. Overflow is possible
-               Result(24 downto 8) :=Result(24 downto 8) + ('0' & Product);
+               --Result <= Result + (x"00" & Product & x"00");
+               Result(24 downto 8) <= Result(24 downto 8) + Product;
          
             -- Multiply first byte of A and B
             when Stage4 =>
                -- add existing value of Q(31 downto 16) with Multiple. Overflow is not possible
-               Result(31 downto 16) := Result(31 downto 16) + Product;
+               --Result <= Result + (Product & x"0000");
+               Result(31 downto 16) <= Result(31 downto 16) + Product;
                
-               Q <= Result;
          end case;
       end if;
    end process Synch;
+
+   Product <= LHS * RHS;
+   Q <= Result;
 
 end architecture Behavioral;
